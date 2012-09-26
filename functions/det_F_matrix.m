73c719cfbcd8712f1da844bd0f17127783664d58
Bernard Ángel Hernández Pérez <bahp@csc.kth.se>
2012-09-26 15:39:05 +0200

    ~ det_F_matrix normalization as parameter

--- functions/det_F_matrix.m
+++ functions/det_F_matrix.m
@@ -13,10 +13,50 @@
 % Output: F (3,3) with the last singular value 0
 % 
 
-function F = det_F_matrix(points1, points2)
+function F = det_F_matrix(pA, pB, NORM)
 
-%------------------------------
-%
-% FILL IN THIS PART
-%
-%------------------------------
+%-------------------------------------------------------------------------
+%                        FILL IN THIS PART
+%-------------------------------------------------------------------------
+%NORM = 1; % Variable to set if normalize or not.
+
+if NORM
+    % Points normalized.
+    [norm_matA] = get_normalization_matrices(pA);
+    pA_norm = norm_matA*pA;
+
+    [norm_matB] = get_normalization_matrices(pB);
+    pB_norm = norm_matB*pB;
+else
+    % Points not normalized.
+    pA_norm = pA;
+    pB_norm = pB;
+end
+
+% Data sizes.
+n_1 = size(pA,2);
+
+% Calculate Q (there is no beta, so only alpha, remembering lab 1).
+Q = [pB_norm(1,:).*pA_norm(1,:); pB_norm(1,:).*pA_norm(2,:); pB_norm(1,:); ...
+     pB_norm(2,:).*pA_norm(1,:); pB_norm(2,:).*pA_norm(2,:); pB_norm(2,:); ...
+     pA_norm(1,:); pA_norm(2,:); ones(1,n_1)]';
+ 
+% Compute eigenvectors (V) and eigenvalues (S) of Q.
+[U,S,V] = svd(Q);
+
+% Select the eigenvector h0 with minimun eigenvalue -> H
+F_norm = reshape(V(:,end),3,3)';
+
+% Undo the normalization (only if we did it).
+if NORM  F_no_properties = norm_matB'*F_norm*norm_matA;
+else     F_no_properties = F_norm;
+end
+
+% Fulfill the property rank = 2.
+if (rank(F_no_properties) > 2 )
+    k = 2; % rank.
+    [U,S,V] = svd(F_no_properties);
+    F = U(:, 1:k)* S(1:k, 1:k)* V(:, 1:k)';
+else
+    F = F_no_properties;
+end
